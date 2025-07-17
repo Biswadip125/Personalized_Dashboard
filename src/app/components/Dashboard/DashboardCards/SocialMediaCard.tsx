b0700681847c2ec7265fac5e0a60da6ffc7fab8c
Biswadip125 <162881134+Biswadip125@users.noreply.github.com>
2025-07-17 09:34:21 +0530

    Social Media Card compatible for dark mode

--- src/app/components/Dashboard/DashboardCards/SocialMediaCard.tsx
+++ src/app/components/Dashboard/DashboardCards/SocialMediaCard.tsx
@@ -7,19 +7,25 @@ const SocialMediaCard = ({
   socialMediaPost: SocialPost;
 }) => {
   return (
-    <div className="flex flex-col gap-2 bg-white p-4 rounded-2xl ">
-      {/*User Id */}
+   <div
+      className={`flex flex-col gap-2 p-4 rounded-2xl ${
+        isDark ? "bg-gray-700/60 text-white" : "bg-white text-black"
+      }`}
+    >
+      {/* User Id */}
       <h4 className="text-lg text-blue-500">@{socialMediaPost.username}</h4>
-      {/*Post Content */}
+
+      {/* Post Content */}
       <p>{socialMediaPost.content}</p>
-      {/*Meta Data */}
-      <div className=" flex flex-col gap-1">
+
+      {/* Meta Data */}
+      <div className="flex flex-col gap-1">
         {socialMediaPost.hashTags.map((hashtag: string, index: number) => (
           <span key={index} className="text-blue-500">
             {hashtag}
           </span>
         ))}
-        <span>
+        <span className={`${isDark ? "text-gray-300" : "text-gray-700"}`}>
           Posted On:{" "}
           {new Date(socialMediaPost.createdAt).toISOString().split("T")[0]}
         </span>
